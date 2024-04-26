d4c5277661a74d86cf2107bf1b88c0d41ed0d80d
Aniket6181 <emailid>
2024-04-26 05:21:04 +0000

    TestNg classes

--- src/test/java/testScript/GooglePageTest.java
+++ src/test/java/testScript/GooglePageTest.java
@@ -1,13 +1,80 @@
 package testScript;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class GooglePageTest {
+	WebDriver driver;
+	
+	@BeforeTest
+	//@BeforeMethod
+	public void setup() {
+		driver= new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+	
+	
+	  @Test (priority=2)
+	  public void javaSearchTest(){
+	  driver.get("https://www.google.com");
+	  WebElement srchBox =driver.findElement(By.className("gLFyf"));
+	  srchBox.sendKeys("Java Tutorial"); srchBox.submit();
+	  Assert.assertEquals(driver.getTitle(), "Java Tutorial - Google Search");
+	  
+	  }
+	 
+	 
+  
+		/*
+		 * @Test public void javaSearchTest(){ driver.get("https://www.google.com");
+		 * SoftAssert softAssert = new SoftAssert();
+		 * softAssert.assertEquals(driver.getTitle(), "Google Page"); WebElement srchBox
+		 * =driver.findElement(By.className("gLFyf"));
+		 * srchBox.sendKeys("Java Tutorial"); srchBox.submit();
+		 * softAssert.assertEquals(driver.getTitle(), "Java Tutorial - Google Search");
+		 * softAssert.assertAll(); }
+		 */
+  
+  @Test(priority=1)
+  public void seleniumSearchTest(){
+	  driver.get("https://www.google.com");
+	  WebElement srchBox =driver.findElement(By.className("gLFyf"));
+	  srchBox.sendKeys("Selenium Tutorial");
+	  srchBox.submit();
+	  Assert.assertEquals(driver.getTitle(), "Selenium Tutorial - Google Search");
+	  
+  }
+  
   @Test
-  public void javaSearchTest(){
-	  WebDriver driver = new ChromeDriver();
-	  driver.manage().window().maximize();
+  public void cucumberSearchTest(){
+	  driver.get("https://www.google.com");
+	  WebElement srchBox =driver.findElement(By.className("gLFyf"));
+	  srchBox.sendKeys("Cucumber Tutorial");
+	  srchBox.submit();
+	  Assert.assertEquals(driver.getTitle(), "Cucumber Tutorial - Google Search");
+	  
+  }
+  
+  @Test
+  public void appiumSearchTest(){
+	  driver.get("https://www.google.com");
+	  WebElement srchBox =driver.findElement(By.className("gLFyf"));
+	  srchBox.sendKeys("Appium Tutorial");
+	  srchBox.submit();
+	  Assert.assertEquals(driver.getTitle(), "Appium Tutorial - Google Search");
+	  
+  }
+  @AfterTest
+  //@AfterMethod
+  public void teardown() {
+	  driver.close();
   }
 }
